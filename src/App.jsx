36c0ee44fd9cb6d6ff5877a2331fd5825e14b874
Md Asadul Islam Nayeem <asadulnayeem99@gmail.comm>
2024-01-03 00:02:54 +0600

    another

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,6 @@ const App = () => {
   const [value, setValue] = useState(0);
   return (
     <CounterContext.Provider value={[value, setValue]}>
-      {/* <Counter />
-      <Count />
-      <Fun />
-      <Time /> */}
-      {/* <Header /> */}
-      {/* <Boiling /> */}
       <Temprature />
     </CounterContext.Provider>
   );
